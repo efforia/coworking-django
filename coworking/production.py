f4eb7448f1a3b083e86f6ceff781bc407eb31bc0
Ubuntu <azureuser@flyingdutchman.flyingdutchman.n6.internal.cloudapp.net>
2016-05-11 19:01:43 +0000

    Added singular postgres database and external compose deployment

--- coworking/production.py
+++ coworking/production.py
@@ -8,40 +8,11 @@ NEVERCACHE_KEY = "_71ak9!4$&v$w2=*!3%^e!h9o4ac@^$gecykhf5-4%+t1=jn!k"
 
 DATABASES = {
     "default": {
-        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        # DB name or path to database file if using sqlite3.
-        "NAME": "postgres",
-        # Not used with sqlite3.
+        "NAME": "fabrica",
         "USER": "postgres",
-        # Not used with sqlite3.
         "PASSWORD": "",
-        # Set to empty string for localhost. Not used with sqlite3.
         "HOST": "db",
-        # Set to empty string for default. Not used with sqlite3.
         "PORT": "5432",
     }
 }
-
-###################
-# DEPLOY SETTINGS #
-###################
-
-# Domains for public site
-# ALLOWED_HOSTS = [""]
-
-# These settings are used by the default fabfile.py provided.
-# Check fabfile.py for defaults.
-
-# FABRIC = {
-#     "DEPLOY_TOOL": "rsync",  # Deploy with "git", "hg", or "rsync"
-#     "SSH_USER": "",  # VPS SSH username
-#     "HOSTS": [""],  # The IP address of your VPS
-#     "DOMAINS": ALLOWED_HOSTS,  # Edit domains in ALLOWED_HOSTS
-#     "REQUIREMENTS_PATH": "requirements.txt",  # Project's pip requirements
-#     "LOCALE": "en_US.UTF-8",  # Should end with ".UTF-8"
-#     "DB_PASS": "",  # Live database password
-#     "ADMIN_PASS": "",  # Live admin user password
-#     "SECRET_KEY": SECRET_KEY,
-#     "NEVERCACHE_KEY": NEVERCACHE_KEY,
-# }
---
+++ deploy/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python manage.py collectstatic --noinput 
+# Verify if the database exists on Postgres
+if !(psql -U postgres -h db -lqt | cut -d \| -f 1 | grep -qw fabrica); then
+	psql -U postgres -h db -c "create database fabrica;" 
+fi
+python manage.py makemigrations --settings="coworking.production"
+python manage.py migrate --settings="coworking.production" 
+gunicorn coworking.wsgi -b 0.0.0.0:8000
