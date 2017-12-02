b3f7b787df6c548e44398137b2afef7542ffa049
William Oliveira de Lagos <william.lagos@live.com>
2017-12-02 01:00:13 -0200

    Cleaned and fixed source code for MySQL + Django + Gunicorn config

--- coworking/production.py
+++ coworking/production.py
@@ -1,18 +1,18 @@
 from coworking.settings import *
 
-DEBUG = False
+DEBUG = True
 
 # Make these unique, and don't share it with anybody.
 SECRET_KEY = "qj@*uxslwnu77y*@yv12$4@12$$7_5!)276&aohn$3c2kj=hjs"
 NEVERCACHE_KEY = "_71ak9!4$&v$w2=*!3%^e!h9o4ac@^$gecykhf5-4%+t1=jn!k"
 
-# DATABASES = {
-    # "default": {
-        # "ENGINE": "django.db.backends.postgresql_psycopg2",
-        # "NAME": "fabrica",
-        # "USER": "postgres",
-        # "PASSWORD": "",
-        # "HOST": "db",
-        # "PORT": "5432",
-    # }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "fabrica",
+        "USER": "root",
+        "PASSWORD": "mk28to#$",
+        "HOST": "localhost",
+        "PORT": "3306",
+    }
+}
--- coworking/wsgi.py
+++ coworking/wsgi.py
@@ -4,17 +4,13 @@ WSGI config for coworking project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
-# from mezzanine.utils.conf import real_project_name
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coworking.production")
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      # "%s.production" % real_project_name("coworking"))
 
 application = get_wsgi_application()
